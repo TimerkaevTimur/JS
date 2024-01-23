640f81b3f5660022e2014c538f6cdce86b2339d2
TimerkaevTimur <144235555+TimerkaevTimur@users.noreply.github.com>
2024-01-23 11:33:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,82 @@
+// /// Работа переменные
+ 
+// let num = 3; 
+// alert(num); 
+ 
+// let a = 10; 
+// let b = 2; 
+ 
+// console.log(a + b); 
+// console.log(a - b); 
+// console.log(a / b); 
+// console.log(a * b); 
+ 
+// let c = 15; 
+// let d = 2; 
+ 
+// result = c + d; 
+// console.log(result); 
+ 
+// let a = 10; 
+// let b = 2; 
+// let c = 5; 
+ 
+// alert(a + b + c); 
+ 
+// let a = 17; 
+// let b = 10; 
+ 
+// let c = a - b; 
+// let d = c; 
+ 
+// console.log(c); 
+ 
+// /// строки
+ 
+// let str = 'Привет мир!'; 
+// alert(str) 
+ 
+// let str1 = 'Привет, '; 
+// let str2 = 'Мир!'; 
+ 
+// alert(str1 + str2); 
+ 
+// let name = 'Arsenii'; 
+// console.log(`hello ${name}`); 
+ 
+// let age = 17; 
+// console.log(`тебе ${age} лет!`); 
+ 
+// /// prompt 
+ 
+// let name = prompt('Как тебя зовут?', ); 
+// alert(`Ваше имя ${name}`); 
+ 
+// let x = prompt('Введите любое число!', ); 
+// alert(x * x); 
+ 
+// /// Обращение к символом  
+ 
+// let str = 'abcde'; 
+// console.log(str[0] + str[2] + str[4]) 
+ 
+// let num = '12345'; 
+// console.log(num[0] * num[1] * num[2] * num[3] * num[4]); 
+ 
+// /// Практика 
+ 
+// let x = 1; // час 
+// let y = 24; // день 
+// let z = 720; // месяц 
+ 
+// console.log(`В одном часе ${x * 60 * 60} сек, так же в одном дне ${y * 60 * 60} сек и в одном месяце ${z * 60 * 60} сек`) 
+ 
+// let x = 10 // час 
+// let y = 57 // мин 
+// let z = 12 // сек 
+ 
+// console.log(`${x}:${y}:${z}`); 
+ 
+// let x = 12; 
+// c = x * x 
+// console.log(c)
